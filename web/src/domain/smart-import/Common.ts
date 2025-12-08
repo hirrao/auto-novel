841ced2335e5e5a30b2eed71647a3a8da8804f34
FishHawk <whang23@outlook.com>
2025-12-09 00:42:57 +0800

    fix(web): 修复亚马逊封面重整函数

--- web/src/domain/smart-import/Common.ts
+++ web/src/domain/smart-import/Common.ts
@@ -7,4 +7,5 @@ export const prettyCover = (cover: string) =>
   cover
     .replace('_PJku-sticker-v7,TopRight,0,-50.', '')
     .replace('m.media-amazon.com', 'images-cn.ssl-images-amazon.cn')
-    .replace(/\.[A-Z0-9_]+\.jpg$/, '.jpg');
+    .replace(/\.[A-Z0-9_]+\.jpg$/, '.jpg')
+    .replace(/\.[A-Z0-9_]+ControlCacheEqualizer\.jpg$/, '.jpg');
